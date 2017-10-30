1516b033533e347a86b1849c2bbe355c885a4929
solarizedtech <33233465+solarizedtech@users.noreply.github.com>
2017-10-30 20:39:21 +0100

    Add files via upload

---
+++ nextgen_index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Scout Me Online</title>
+
+    <!-- Bootstrap -->
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <nav class="navbar navbar-default navbar-inverse" id=bs-navbar>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand" href="index.php">ScoutMeOnline</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Players</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> LOGIN</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> SIGN UP</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-info-sign"></span> ABOUT US</a></li>
+        </ul>
+      </div>
+    </div>
+</nav>
+    <div class="row">
+    <div class="container-fluid">
+          <div class="jumbotron col-sm-12 col-md-12 col-lg-12 col-xs-12 text-center">
+              <p>Reason why we are in business? To help the upcoming Nigerian football players.... </p><br>
+              <p>New User? Select from the profiles below to see how our website can serve you.</p>
+          </div>
+          <div class="jumbotron col-sm-6 col-md-6 col-lg-6 col-xs-6">
+              <h1 class="text-center">Football player</h1>
+              <button class="btn btn-link"><a href="#">Learn more</a></button>
+          </div>
+          <div class="jumbotron col-sm-6 col-md-6 col-lg-6 col-xs-6">
+              <h1 class="text-center">Scout or Agent</h1>
+              <button class="btn btn-link"><a href="#">Learn more</a></button>
+          </div>
+          <!-- <div class="jumbotron col-sm-6 col-md-6 col-lg-6 col-xs-6"></div> -->
+      </div>
+    </div>
+
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+    <script
+    src="https://code.jquery.com/jquery-1.12.4.min.js"
+    integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+    crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- <script src="js/bootstrap.min.js"></script> -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+  </html>
